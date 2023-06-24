739c547ae218c7e770d7705aa9e0ea7c21938c0c
Mingcong Li <ge95xom@mytum.de>
2023-06-24 17:25:54 +0200

    update 3d object detection

--- object_detect/Object_detection.py
+++ object_detect/Object_detection.py
@@ -4,7 +4,6 @@ import ros_numpy
 import mediapipe as mp
 """
 $ pip install -q mediapipe==0.10.0
-$ wget -q -O efficientdet.tflite -q https://storage.googleapis.com/mediapipe-models/object_detector/efficientdet_lite0/float32/latest/efficientdet_lite0.tflite
 """
 from scipy.spatial.transform import Rotation as R
 
