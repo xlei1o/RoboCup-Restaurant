496f685600551711cd9dd5b8a2a65e5cfe346946
Xiaodong Lei <x.lei@tum.de>
2023-06-29 23:08:11 +0200

    finish speech recognition

--- restaurant/scripts/restaurant/run.py
+++ restaurant/scripts/restaurant/run.py
@@ -5,6 +5,7 @@ import smach_ros
 
 from restaurant import states
 from hand_detection.hand_detection_node import HandDetection
+from wit_ros.wit_node import WitRos
 
 # main
 def main():
@@ -18,9 +19,14 @@ def main():
     _storeageTable = rospy.get_param('/fixed_coordinates/storage_table')
     _leftTable = rospy.get_param('/fixed_coordinates/table_left')
     _rightTable = rospy.get_param('/fixed_coordinates/table_right')
-    hd = HandDetection()
+
+    WIT_API_KEY = rospy.get_param('/wit_api_key')
+    ########## Starting Task Manager #######
+    hd = HandDetection()  #hand detection
     hd.listen()
 
+    wr = WitRos(WIT_API_KEY)  # speech recognition
+    wr.start()
 
     # Open the container
     with sm:
@@ -66,7 +72,7 @@ def main():
         #                        transitions={'success': 'HEAR',
         #                                     'failure': 'failure'})
         
-        # smach.StateMachine.add('HEAR', states.Hear(),
+        # smach.StateMachine.add('HEAR', states.Hear(wr),
         #                        transitions={'success': 'CHECK',
         #                                     'failure': 'ASK'})
         
@@ -95,30 +101,41 @@ def main():
         # smach.StateMachine.add('CK', states.CheckObjExist('bottle'),
         #                        transitions={'success': 'failure',
         #                                     'failure': 'failure'})
-        smach.StateMachine.add('LOOK_LEFT', states.LookAround(_leftTable),
-                               transitions={'success': 'CALLING_LEFT',
+        # smach.StateMachine.add('LOOK_LEFT', states.LookAround(_leftTable),
+        #                        transitions={'success': 'CALLING_LEFT',
+        #                                     'failure': 'failure'})
+        # smach.StateMachine.add('LOOK_RIGHT', states.LookAround(_rightTable),
+        #                        transitions={'success': 'CALLING_RIGHT',
+        #                                     'failure': 'failure'})
+        # smach.StateMachine.add('CALLING_LEFT', states.Calling(hd),
+        #                        transitions={'success': 'NAVAGATION_LEFT',
+        #                                     'failure': 'LOOK_RIGHT'})
+        # smach.StateMachine.add('CALLING_RIGHT', states.Calling(hd),
+        #                        transitions={'success': 'failure',
+        #                                     'failure': 'LOOK_LEFT'})
+        # smach.StateMachine.add('NAVAGATION_LEFT', states.Navigation(_leftTable),
+        #                        transitions={'success': 'ASK',
+        #                                     'failure': 'failure'})
+        # smach.StateMachine.add('ASK', states.Say('Hello what can I do for you'),
+        #                        transitions={'success': 'CHECK',
+        #                                     'failure': 'failure'})
+        # smach.StateMachine.add('CHECK', states.CheckObjExist('apple'),
+        #                        transitions={'success': 'failure',
+        #                                     'failure': 'failure'})
+        smach.StateMachine.add('NAV_TO_STOREAGE', states.Navigation(_storeageTable),
+                               transitions={'success': 'OBJ_DETECTION',
                                             'failure': 'failure'})
-        smach.StateMachine.add('LOOK_RIGHT', states.LookAround(_rightTable),
-                               transitions={'success': 'CALLING_RIGHT',
+        smach.StateMachine.add('OBJ_DETECTION', states.ObjectDetection(),
+                               transitions={'success': 'STAND_BY',
                                             'failure': 'failure'})
-        smach.StateMachine.add('CALLING_LEFT', states.Calling(hd),
-                               transitions={'success': 'NAVAGATION_LEFT',
-                                            'failure': 'LOOK_RIGHT'})
-        smach.StateMachine.add('CALLING_RIGHT', states.Calling(hd),
-                               transitions={'success': 'failure',
-                                            'failure': 'LOOK_LEFT'})
-        smach.StateMachine.add('NAVAGATION_LEFT', states.Navigation(_leftTable),
+        smach.StateMachine.add('STAND_BY', states.Navigation(_standby),
                                transitions={'success': 'ASK',
                                             'failure': 'failure'})
-        smach.StateMachine.add('ASK', states.Say('Hello what can I do for you'),
-                               transitions={'success': 'CHECK',
-                                            'failure': 'failure'})
-        smach.StateMachine.add('CHECK', states.CheckObjExist('apple'),
+        smach.StateMachine.add('ASK', states.Conversation('Hello what can I do for you'),
                                transitions={'success': 'failure',
                                             'failure': 'failure'})
-        
     # Use a introspection for visulize the state machine
-    sm.set_initial_state(['LOOK_LEFT'])
+    sm.set_initial_state(['NAV_TO_STOREAGE'])
 ###########################################################DEBUG###############################################################
 
     sis = smach_ros.IntrospectionServer('example_server', sm, '/SM_ROOT')
--- restaurant/scripts/restaurant/states.py
+++ restaurant/scripts/restaurant/states.py
@@ -1,20 +1,14 @@
 import rospy
 import smach
 import actionlib
-import cv2
 
-from std_msgs.msg import Float64
+
 from control_msgs.msg import PointHeadActionGoal
-from geometry_msgs.msg import PointStamped
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
-from pal_interaction_msgs.msg import TtsAction, TtsGoal
-from control_msgs.msg import FollowJointTrajectoryAction, FollowJointTrajectoryGoal
-from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from darknet_ros_msgs.msg import BoundingBoxes
 from objects_msgs.msg import objects, single
-
+from wit_ros.srv import ListenAndInterpret, ListenAndInterpretResponse
 from restaurant import functions
-
 
 class Say(smach.State):
     def __init__(self, text):
@@ -68,20 +62,6 @@ class LookAround(smach.State):
         self.table = table
         self.pub_head_topic = rospy.Publisher('/head_controller/point_head_action/goal', PointHeadActionGoal, queue_size=1)
     def execute(self, userdata):
-        # client = actionlib.SimpleActionClient('/head_controller/follow_joint_trajectory', FollowJointTrajectoryAction)
-        # client.wait_for_server()
-        # goal = FollowJointTrajectoryGoal()
-        # goal.trajectory.joint_names = ['head_1_joint', 'head_2_joint']
-        # point = JointTrajectoryPoint()
-        # point.positions = [self.pan_angle, 0.0]
-        # point.time_from_start = rospy.Duration(1.0)
-        # goal.trajectory.points.append(point)
-        # client.send_goal(goal)
-        # client.wait_for_result()
-        # if client.get_state() == actionlib.GoalStatus.SUCCEEDED:
-        #     return 'success'
-        # else:
-        #     return 'failure'
         phag = PointHeadActionGoal()
         phag.header.frame_id = "/map"
         phag.goal.max_velocity = 1.0
@@ -92,8 +72,6 @@ class LookAround(smach.State):
 
         phag.goal.target.point.x = self.table['x']
         phag.goal.target.point.y = self.table['y']
-        # phag.goal.target.point.x = 1.23
-        # phag.goal.target.point.y = 1.94
         phag.goal.target.point.z = 0.9
         # rospy.loginfo("Sending: " + str(phag))
         self.pub_head_topic.publish(phag)
@@ -102,10 +80,12 @@ class LookAround(smach.State):
         
 class ObjectDetection(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['success', 'failure','preempted'],input_keys=['grasp_ready', 'exist_objects'], output_keys=['exist_objects'])
+        smach.State.__init__(self, outcomes=['success', 'failure','preempted'],
+                             input_keys=['grasp_ready', 'exist_objects'], 
+                             output_keys=['exist_objects'])
         self.object_pub = rospy.Publisher('/restaurant/objects', objects, queue_size=10)
         self.names = None
-        _ = functions.HeadAction(0.0, -0.8)
+        functions.HeadAction(0.0, -0.8)
 
     def callback(self, box):
         number = len(box.bounding_boxes)
@@ -123,28 +103,47 @@ class ObjectDetection(smach.State):
         self.object_pub.publish(msg)
         
     def execute(self, userdata):
+        rospy.sleep(5)
         rospy.Subscriber('/darknet_ros/bounding_boxes', BoundingBoxes, self.callback)
         rospy.wait_for_message('/darknet_ros/bounding_boxes', BoundingBoxes)
         userdata.exist_objects = self.names
+        functions.HeadAction(0.0, 0.0)
         if userdata.grasp_ready:
-            _ = functions.HeadAction(0.0, 0.0)
+            # functions.HeadAction(0.0, 0.0)
             return 'preempted'
         else:
             return 'success'
 
 
-class CheckObjExist(smach.State):
-    def __init__(self, requre):
+
+            
+class Conversation(smach.State):
+    def __init__(self, text):
         smach.State.__init__(self, outcomes=['success', 'failure'],
                              input_keys=['exist_objects'],
-                             output_keys=['grasp_ready'])
-        self.requre = requre
+                             output_keys=['require_object','grasp_ready'])
+        self.text = text
 
     def execute(self, userdata):
-        if self.requre in userdata.exist_objects:
-            userdata.grasp_ready = True
-            return 'success'
-        else: 
-            _ = functions.Speak('sorry we do not have '+ self.requre)
-            return 'failure'
-            
+        functions.Speak(self.text)
+        rospy.sleep(0.1)
+        while not rospy.is_shutdown():
+            while not rospy.is_shutdown():
+                print(userdata.exist_objects)
+                rospy.wait_for_service('/restaurant/wit/listen_interpret')
+                service_proxy = rospy.ServiceProxy('/restaurant/wit/listen_interpret', ListenAndInterpret)
+                response = service_proxy()
+                if response.result != 'nothing':
+                    break
+                else:
+                    functions.Speak('sorry please say again loudly')
+            if response.result in userdata.exist_objects:
+                functions.Speak('okay ' + response.result + ' please wait for a moment')
+                userdata.require_object = response.result
+                userdata.grasp_ready = True
+                break
+            else:
+                functions.Speak('sorry we do not have '+ response.result + ' please order something else')
+
+        rospy.loginfo(response.result)
+        return 'success'
